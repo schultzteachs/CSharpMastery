4605cb913b7058266525715574c9b884a59205a8
SSSchultz <craigrobertschultz.com>
2026-01-05 17:38:07 -0500

    Started the console app to parse files project

---
+++ 03-App-to-parse-files/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.IO;
+using System.Collections.Generic;
+
+var salesFiles = FindFiles("stores");
+
+foreach (var file in salesFiles)
+{
+    Console.WriteLine(file);
+}
+
+        IEnumerable<string> FindFiles(string folderName)
+{
+    List<string> salesFiles = new List<string>();
+
+    var foundFiles = Directory.EnumerateFiles(folderName, "*", SearchOption.AllDirectories);
+
+    foreach (var file in foundFiles)
+    {
+       
+        if (file.EndsWith("sales.json"))
+        {
+            salesFiles.Add(file);
+        }
+    }
+
+    return salesFiles;
+}
+
+
+
+
+
